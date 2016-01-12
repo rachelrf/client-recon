e778a6da1c4896bccc47d1128289ae985556b5e7
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-12 15:50:12 -0800

    Adds Amazon API to feed-controller.js

--- server/controllers/feed-controller.js
+++ server/controllers/feed-controller.js
@@ -1,48 +1,63 @@
 'use strict';
 var db = require('../../db');
-var bingApi = require('../helpers/bing-search-api.js')
-var weatherApi = require('../helpers/weather-api.js')
+var amazonApi = require('../helpers/amazon-api.js');
+var bingApi = require('../helpers/bing-search-api.js');
+var weatherApi = require('../helpers/weather-api.js');
 var Client = require('../models').Client;
 var moment = require('moment');
 var getBirthdayMessage = require('../helpers/birthdateCalc.js');
 
 module.exports = {
   // FOR TESTING DELETE THIS LATER
-    getOneClient: function(req,res){
-      Client.getOne(req.params.client_id, function(result){ 
-        module.exports.get(req,res, result,function(feedResult){
-          res.json(feedResult);
-        });
+  getOneClient: function(req,res){
+    Client.getOne(req.params.client_id, function(result){ 
+      module.exports.get(req,res, result,function(feedResult){
+        res.json(feedResult);
       });
-    
-    },
+    });
+  
+  },
   //
-  getBing: function(company,cb){
+
+  getAmazon: function(likes, cb) {
+    amazonApi(likes, function(result) {
+      cb(result);
+    });
+  },
+
+  getBing: function(company, cb){
   	bingApi(company, function(result){
   		cb(result.d.results);
-  	})
+  	});
   },
+
   getWeather: function(zipcode, cb){
   	weatherApi(zipcode, function(result){
   		cb(result);
-  	})
-
+  	});
   },
-  get:function(req, res, params, callback) {
-      var feedResults = {};
-      var zipcode = params[0].client_zipcode;
-      var company = params[0].client_company;
-      module.exports.getBing(company, function(bingResults) {
-        console.log("bing results are:", bingResults);
-        feedResults.bing = bingResults;
-        module.exports.getWeather(zipcode, function(weatherResults) {
-          feedResults.weather = weatherResults;
-          ;
+
+  get: function(req, res, params, callback) {
+    //dummy value, while frontend for client's likes are not built out
+    var likes = 'Beyonce';
+    //
+    var feedResults = {};
+    var zipcode = params[0].client_zipcode;
+    var company = params[0].client_company;
+    module.exports.getBing(company, function(bingResults) {
+      console.log("bing results are:", bingResults);
+      feedResults.bing = bingResults;
+      module.exports.getWeather(zipcode, function(weatherResults) {
+        feedResults.weather = weatherResults;
+        module.exports.getAmazon(likes, function(amazonResults) {
+          feedResults.amazon = amazonResults;
+
           // feedResults.message
           feedResults.message = params[0].client_name+"'s birthday is "+ getBirthdayMessage(params[0].client_birthday) +'! Think about '
           + 'how you can make their day special.'
           res.json(feedResults);
         });
       });
-    }
-}
+    });
+  }
+};
