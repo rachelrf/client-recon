2b03ccdb344e89b08677f8bae4a1f40eab323bb3
Gloria Ma <gloria.ma@aya.yale.edu>
2016-01-14 15:16:13 -0800

    Allows cross-origin requests from localhost:8100 (ionic server)

--- server/server.js
+++ server/server.js
@@ -8,6 +8,18 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Allow cross origin requests;
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 //Set up routes
 require('./routes/index.js')(app, express);
 
